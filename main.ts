05f6c5b661ca8d1b12dde9acd3a6dcfbc3d56436
Dario22-eng <124134212+Dario22-eng@users.noreply.github.com>
2023-03-08 09:10:33 +0100

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,20 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber == 2) {
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 255)
+    }
+    if (receivedNumber == 3) {
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 0)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 255)
+    }
+    if (receivedNumber == 1) {
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 0)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 255)
+    }
+    if (receivedNumber == 3) {
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 0)
+    }
+})
+radio.setGroup(1)
+basic.forever(function () {
+	
+})
